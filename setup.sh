f767319042589b8df60a02327e164d56c3ab5a08
ketevankakabadze <ketuna_kakabadze@yahoo.com>
2020-07-30 17:19:55 +0200

    A

--- setup.sh
+++ setup.sh
@@ -4,8 +4,4 @@ echo "[server]
 headless = true
 port = $PORT
 enableCORS = false
-<<<<<<< HEAD
-"~/.streamlit/config.toml
-=======
-" ~/.streamlit/config.toml
->>>>>>> 7ab45400ce802cedae8a01312cd7d0dd6ee4e20d
+" > ~/.streamlit/config.toml
